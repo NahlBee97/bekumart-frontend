e67bf93cf7d90c8113785d08c308c55c0545fa9c
Nahalil <nahalilmuchtar2@gmail.com>
2025-09-26 11:09:14 +0800

    update confirm order logic

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import { apiUrl } from "@/config";
+import axios from "axios";
+import { getCookie } from "cookies-next";
 import { CheckCircleIcon } from "lucide-react";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Success() {
+  useEffect(() => {
+    try {
+      const token = getCookie("access_token") as string;
+
+      if (!token) throw new Error("No access token found");
+
+      const urlParams = new URLSearchParams(window.location.search);
+      const orderId = urlParams.get("order_id");
+
+      const updateOrderStatus = async () => {
+        if (orderId) {
+          await axios.patch(
+            `${apiUrl}/api/orders/${orderId}`,
+            {
+              status: "PROCESSING",
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+        }
+      };
+      updateOrderStatus();
+    } catch (error) {
+      console.error("Error updating order status:", error);
+    }
+  }, []);
+
   return (
     <div className="bg-gray-50 min-h-screen flex items-center justify-center p-4 font-sans">
       <div className="bg-white p-6 md:p-10 rounded-2xl shadow-xl w-full max-w-2xl text-center">
