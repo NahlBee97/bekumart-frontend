14aac45657efab8220c310edcb3cdf2cc5ea4948
Nahalil <nahalilmuchtar2@gmail.com>
2025-09-26 18:30:23 +0800

    order history page

---
+++ src/components/profile/orders/statusBadge.tsx
@@ -0,0 +1,20 @@
+import { IOrder } from "@/interfaces/orderInterface";
+
+// A component to render the status badge with appropriate colors
+const StatusBadge: React.FC<{ status: IOrder["status"] }> = ({ status }) => {
+  const baseClasses = "px-3 py-1 text-sm font-medium rounded-full inline-block";
+  const statusClasses = {
+    PENDING: "bg-yellow-100 text-orange-800",
+    PROCESSING: "bg-yellow-100 text-yellow-800",
+    OUT_FOR_DELIVERY: "bg-blue-100 text-blue-800",
+    READY_FOR_PICKUP: "bg-blue-100 text-blue-800",
+    COMPLETED: "bg-green-100 text-green-800",
+    CANCELLED: "bg-red-100 text-red-800",
+  };
+
+  return (
+    <span className={`${baseClasses} ${statusClasses[status]}`}>{status}</span>
+  );
+};
+
+export default StatusBadge;
